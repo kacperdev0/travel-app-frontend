6ab1cb7cdee31b7780aac5d2c8686ed1003d600a
kacperdev0 <kacperoldziejewski.kontakt@gmail.com>
2024-07-24 18:02:54 +0200

    now hotels are displayed as pointers on the map

--- src/Components/MapComponent.js
+++ src/Components/MapComponent.js
@@ -2,29 +2,20 @@ import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import HotelService from '../API/HotelService';
 import { useEffect, useState } from 'react';
+import SearchBarComponent from './SearchBarComponent';
 
 const MapComponent = () => {
   const [hotels, setHotels] = useState([]);
 
   useEffect(() => {
     const params = {
-      destId: '-2092174',
-      searchType: 'CITY',
-      arrivalDate: '2024-07-22',
-      departureDate: '2024-07-25',
-      adults: 1,
-      childrenAge: '0,17',
-      roomQty: 1,
-      pageNumber: 1,
-      units: 'metric',
-      temperatureUnit: 'c',
-      languageCode: 'en-us',
-      currencyCode: 'AED'
-    };
+      "latitude": "52.229675",
+      "longitude": "21.012230"
+    }
 
     HotelService.getHotels(params).then(res => {
-      setHotels(res.data)
-      console.log(res.data)
+      setHotels(res.data.elements)
+      console.log(res.data.elements)
     }).catch(error => {
       console.error('Error fetching hotels:', error);
     });
@@ -32,6 +23,7 @@ const MapComponent = () => {
 
   return (
     <div style={{ height: '90vh', width: '100%', margin: 0, padding: 0, overflow: 'hidden' }}>
+      
       <MapContainer
         center={[52.2297, 21.0122]}
         zoom={13}
@@ -42,10 +34,9 @@ const MapComponent = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
         {hotels.map((hotel, index) => (
-          <Marker key={index} position={[hotel.latitude, hotel.longitude]}>
+          <Marker key={index} position={[hotel.lat, hotel.lon]}>
             <Popup>
-              {hotel.name}<br />
-              {hotel.address}
+              
             </Popup>
           </Marker>
         ))}
