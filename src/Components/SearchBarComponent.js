ca0c2e21e88437780aa9dfb36505824c2fbd1392
kacperdev0 <kacperoldziejewski.kontakt@gmail.com>
2024-07-30 19:40:16 +0200

    changed MUI autocomplete to TextField to improve code readability

--- src/Components/SearchBarComponent.js
+++ src/Components/SearchBarComponent.js
@@ -1,19 +1,38 @@
 import React, { useState } from 'react';
 import { Autocomplete, TextField, IconButton } from '@mui/material';
 import styles from '../CSS/MapStyle.module.css';
+import axios from 'axios';
 
-const SearchBarComponent = ({setLocation}) => {
+const SearchBarComponent = ({setMainLocation}) => {
   const [suggestions, setSuggestions] = useState(["Test1","Test2","Test3"]);
+  const [location, setLocation] = useState("")
+
+  const handleInputChange = async (event) => {
+    const value = event.target.value;
+    setLocation(value);
+
+    if (value.length > 2) {
+      try {
+        const response = await axios.get(`https://nominatim.openstreetmap.org/search?format=json&q=${value}&addressdetails=1&limit=5`);
+        setSuggestions(response.data);
+      } catch (error) {
+        console.error('Error fetching data from Nominatim API:', error);
+      }
+    } else {
+      setSuggestions([]);
+    }
+  };
 
   return (
     <div className={styles.searchBar}>
-        <Autocomplete
-            disablePortal
-            id="searchbar"
-            options={suggestions}
-            sx={{width: 300}}
-            renderInput={(params) => <TextField {...params} label="Location" />}>
-        </Autocomplete>
+        <TextField 
+          label="Location"
+          variant='outlined'
+          fullWidth
+          value={location}
+          onChange={handleInputChange}
+          />
+         
     </div>
   );
 };
