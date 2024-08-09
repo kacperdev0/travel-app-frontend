d6c7f8d8808d668ffa7bc902714f745a3b136785
kacperdev0 <kacperoldziejewski.kontakt@gmail.com>
2024-08-09 11:56:39 +0200

    now icons style changes when parts of travel plan are selected or null

--- src/Components/PlannerIconsComponent;.js
+++ src/Components/PlannerIconsComponent;.js
@@ -4,7 +4,7 @@ import FlightTakeoffIcon from '@mui/icons-material/FlightTakeoff';
 import FlightLandIcon from '@mui/icons-material/FlightLand';
 import KingBedIcon from '@mui/icons-material/KingBed';
 
-const PlannerIconsComponent = (hotel, chooseHotel, airportDeparture, chooseAirportDeparture, airportArrival, chooseAirportArrival) => {
+const PlannerIconsComponent = ({hotel, chooseHotel, airportDeparture, chooseAirportDeparture, airportArrival, chooseAirportArrival}) => {
   const blueBackground = {
     backgroundColor: "#1976d2",
   }
@@ -17,18 +17,18 @@ const PlannerIconsComponent = (hotel, chooseHotel, airportDeparture, chooseAirpo
     <div style={{ width: '100%' }}>
       <Grid container spacing={2}>
         <Grid textAlign="center" item xs={2.5}>
-          <Paper style={hotel && blueBackground} >
-            <IconButton><KingBedIcon style={hotel && whiteIcon}/></IconButton>
+          <Paper style={hotel ? blueBackground : {}} >
+            <IconButton><KingBedIcon style={hotel ? whiteIcon : {}}/></IconButton>
           </Paper>
         </Grid>
         <Grid textAlign="center" item xs={2.5}>
-          <Paper>
-          <IconButton variant="contained"><FlightTakeoffIcon/></IconButton>
+          <Paper style={hotel ? blueBackground : {}} >
+          <IconButton variant="contained"><FlightTakeoffIcon style={hotel ? whiteIcon : {}}/></IconButton>
           </Paper>
         </Grid>
         <Grid textAlign="center" item xs={2.5}>
-            <Paper>
-                <IconButton><FlightLandIcon/></IconButton>  
+            <Paper style={hotel ? blueBackground : {}} >
+                <IconButton><FlightLandIcon style={hotel ? whiteIcon : {}}/></IconButton>  
             </Paper>
         </Grid>
         <Grid textAlign="center" item xs={4.5}>
