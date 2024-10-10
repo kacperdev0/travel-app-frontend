676c8e39c679ab3010ec18c222f003e99e6fd673
kacperdev0 <kacperoldziejewski.kontakt@gmail.com>
2024-10-10 21:27:28 +0200

    now airports have different style in LocationsList than hotels

--- src/Components/PlanPage/SingleLocationComponent.js
+++ src/Components/PlanPage/SingleLocationComponent.js
@@ -6,7 +6,7 @@ import CompareArrowsIcon from '@mui/icons-material/CompareArrows';
 const SingleLocationComponent = ({ location }) => {
 
   return (
-    <Paper elevation={3} mb="10vh" sx={{ p: "2vh", mb: "1vh" }}>
+    <Paper elevation={3} mb="10vh" sx={location.tags.aeroway == "aerodrome" ? ({ p: "1.5vh", background: "#f1efef" }) : ({ p: "2vh", mt: "1vh" })}>
       <Grid container direction="row" alignItems="center" justifyContent="space-between">
         <Grid item>
           {location.tags.name}
